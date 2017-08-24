42afae11e17afdba9477c6f010f691e67bcb9508
Jay Manligas <jaymanligas@gmail.com>
2017-08-25 07:45:12 +0800

    test

--- messages/index.js
+++ messages/index.js
@@ -54,9 +54,9 @@ bot.dialog('/', [
     function (session, results) {
         session.userData.desiredDate = results.response.entity;
         
-        ugbroka.addReferrer('203181', 'CARDIO', '01010110', '2017-08-28',function(res){
-            console.log(res)
-        });
+        // ugbroka.addReferrer('203181', 'CARDIO', '01010110', '2017-08-28',function(res){
+        //     console.log(res)
+        // });
 
         builder.Prompts.choice(session, "Please choose desired hospital and doctor.", ["Site A - Dr. Dickson Martin", "Site A - Dr. Erwing Sandra", "Site B - Dr. Schwarz Marc"], {listStyle: builder.ListStyle.button});
     },
